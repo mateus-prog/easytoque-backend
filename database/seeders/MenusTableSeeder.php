81a93c7c9df8a28c322543de953b737a3f66376e
Mateus Guizelini <mateus.guizelini@tech.math.group>
2022-07-20 01:04:37 -0300

    ajuste banco de dados

--- database/seeders/MenusTableSeeder.php
+++ database/seeders/MenusTableSeeder.php
@@ -23,12 +23,11 @@ class MenusTableSeeder extends Seeder
             ['id' => 3, 'name' => 'Comissões', 'icon' => 'fas fa-hand-holding-usd', 'url' => '/commissions'],
             ['id' => 4, 'name' => 'Alterar Logo', 'icon' => 'fas fa-image', 'url' => ''],
             ['id' => 5, 'name' => 'Materiais Divulgação', 'icon' => 'fas fa-bullhorn', 'url' => 'https://www.easytoque.com.br/materiais-didaticos/'],
-            ['id' => 6, 'name' => 'Copiar link da loja', 'icon' => 'fas fa-link', 'url' => ''],
-            ['id' => 7, 'name' => 'Contrato', 'icon' => 'fas fa-file-contract', 'url' => ''],
-            ['id' => 8, 'name' => 'Solicitações', 'icon' => 'fas fa-hand-holding-usd', 'url' => '/requests'],
-            ['id' => 9, 'name' => 'Parceiros', 'icon' => 'fas fa-users', 'url' => '/partners'],
-            ['id' => 10, 'name' => 'Logs', 'icon' => 'fas fa-clock', 'url' => '/logs'],
-            ['id' => 11, 'name' => 'Administradores', 'icon' => 'fas fa-user-tie', 'url' => '/administrators'],
+            ['id' => 6, 'name' => 'Contrato', 'icon' => 'fas fa-file-contract', 'url' => ''],
+            ['id' => 7, 'name' => 'Solicitações', 'icon' => 'fas fa-hand-holding-usd', 'url' => '/requests'],
+            ['id' => 8, 'name' => 'Parceiros', 'icon' => 'fas fa-users', 'url' => '/partners'],
+            ['id' => 9, 'name' => 'Logs', 'icon' => 'fas fa-clock', 'url' => '/logs'],
+            ['id' => 10, 'name' => 'Administradores', 'icon' => 'fas fa-user-tie', 'url' => '/administrators'],
         ]);
     }
 }
