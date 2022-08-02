eb1f923c7f6b3c9d9ed5d885fe63a4dc53231f06
Mateus Guizelini <mateus.guizelini@tech.math.group>
2022-08-01 22:51:56 -0300

    adicionando rota

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
 */
+
+Route::get('/env', function () {
+    dd(env('APP_URL'));
+});
