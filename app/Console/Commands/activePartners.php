040f1ab6c6a284a2f5ec302e5cfef38453a16dad
Mateus Guizelini <mateus.guizelini@tech.math.group>
2022-09-07 10:58:46 -0300

    incluindo hash no hash_id e no password

--- app/Console/Commands/activePartners.php
+++ app/Console/Commands/activePartners.php
@@ -9,6 +9,8 @@ use App\Services\ClickSign\ClickSignService;
 use App\Services\Mail\MailService;
 use App\Services\Store\StoreService;
 
+use Illuminate\Support\Facades\Hash;
+
 class activePartners extends Command
 {
     /**
@@ -58,6 +60,20 @@ class activePartners extends Command
         foreach($users as $user)
         {
             //verifica se o usuario é Parceiro e se o status esta pendente
+            if($user->role_id == 4 && $user->senha_hash == 0)
+            {
+                $password = Hash::make($user->password);
+                $hash_id = str_replace('/', '', Hash::make($user->email));
+
+                $data = array(
+                    'password' => $password,
+                    'hash_id' => $hash_id,
+                );
+
+                $this->userService->update($user->id, $data);
+            }
+
+            //verifica se o usuario é Parceiro e se o status esta pendente
             if($user->role_id == 4 && $user->status_user_id == 3)
             {
                 $this->clickSignService->viewDocument($user->id);
@@ -65,22 +81,36 @@ class activePartners extends Command
                 $user = $this->userService->findById($user->id);
                 if($user->status_user_id == 1)
                 {   
+                    $clientIdMax = 0;
+                    $usersStore = $this->userStoreService->all();
+                    foreach($usersStore as $userStore){
+                        $clientId = $userStore->client_id;
+
+                        if($clientId != '' && $clientId != null){
+                            $clientIdMax = $clientIdMax < $clientId ? $clientId : $clientIdMax;
+                        }
+                    }
+
+                    $clientIdMax++;
+
                     //incluir a loja no banco de dados
-                    //$response = $this->storeService->createStoreMagento($user->id);
-                    //$storeId = $response['loja_id'];
-                    $storeId = 1;
+                    $response = $this->storeService->createStoreMagento($user->id);
+                    $storeId = $response['loja_id'];
                     
                     $id = $this->userStoreService->getUserStoreByUser($user->id);
                     $data = array(
-                        'store_id' => $storeId
+                        'store_id' => $storeId,
+                        'client_id' => $clientIdMax,
                     );
+
+                    $store = $this->userStoreService->findById(strval($id));
 
                     $this->userStoreService->update($id, $data);
                     
                     //sendMail complete register user
                     $mailRecipient = $user->email;
                     
-                    $linkStore = 'https://loja.easytoque.com.br/?___store=loja_'.$user->id;
+                    $linkStore = 'https://loja.easytoque.com.br/?___store=loja_'.$store->client_id;
 
                     //mail welcome
                     $mailBody = $this->mailService->createMailPartnerAddFinish($user->first_name, $mailRecipient, $linkStore);
