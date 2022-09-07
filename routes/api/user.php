a1aab9b28d988d19019fd9075f6302689489beb6
Mateus Guizelini <mateus.guizelini@tech.math.group>
2022-09-07 16:28:57 -0300

    ajuste na rota

--- routes/api/user.php
+++ routes/api/user.php
@@ -13,7 +13,8 @@ use App\Http\Controllers\UserBankController;
 |
 */
 
-Route::get('/users/corporate/hash', [UserCorporateController::class, 'getUserHash']);
+//retirar essa rota
+//Route::get('/users/corporate/hash', [UserCorporateController::class, 'getUserHash']);
 Route::get('/users/corporate/{userId}/edit', [UserCorporateController::class, 'getUserCorporateEditByUser']);
 Route::get('/users/corporate/{userId}', [UserCorporateController::class, 'getUserCorporateByUser']);
 
