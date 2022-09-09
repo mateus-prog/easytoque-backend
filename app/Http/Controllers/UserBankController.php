83b2ed33bf87740aa10a1082f2b85cef7b0f5178
Mateus Guizelini <mateus.guizelini@tech.math.group>
2022-09-09 11:45:37 -0300

    ajuste ao salvar dados bancarios

--- app/Http/Controllers/UserBankController.php
+++ app/Http/Controllers/UserBankController.php
@@ -63,7 +63,7 @@ class UserBankController extends Controller
 
             $this->userService->update($id, $input);
 
-            if($user->status_user_id != 1 && $user->id == Auth::user()->id){
+            if($user->status_user_id != 1){
                 $userCorporate = $this->userCorporateService->getUserCorporateByUser($user->id);
                 $userCorporate = $userCorporate[0];
                 $mailManager = 'parceiros+00@toquecolor.com.br';
