e9fb13c01bc4e990a3a6a50ddd82f54088c3f7d6
Mateus Guizelini <mateus.guizelini@tech.math.group>
2022-09-12 01:10:04 -0300

    tratamento de erro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -86,7 +86,7 @@ class AuthController extends Controller
 
     public function reset(Request $request)
     {
-        //try {
+        try {
             $password = $this->generatePassword();
             $user = $this->userService->findByMail($request['email']);
 
@@ -112,9 +112,9 @@ class AuthController extends Controller
             }else{
                 return response()->noContent();
             }
-        /*} catch (Exception $e) {
+        } catch (Exception $e) {
             return $this->error($e->getMessage(), 500);
-        }*/
+        }
     }
 
     function generatePassword($qtyCaraceters = 8)
